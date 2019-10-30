757b6e470b57c8d6c0181895b243aca0abd58881
Bil <bilroum@gmail.com>
2019-10-30 19:11:35 +0100

    Added the moment.js and vue-scroll

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,10 +3,10 @@
     <h2 class="teal-text center">Ninja Chat</h2>
     <div class="card">
       <div class="card-content">
-        <ul class="messages">
+        <ul class="messages" v-chat-scroll>
           <li v-for="message in messages" :key="message.id">
             <span class="teal-text">{{message.name}}</span>
-            <span class="grey-text">{{message.content}}</span>
+            <span class="grey-text text-darken-1">{{message.content}}</span>
             <span class="grey-text time">{{message.timestamp}}</span>
           </li>
         </ul>
@@ -21,6 +21,7 @@
 <script>
 import NewMessage from "@/components/NewMessage";
 import db from "@/firebase/init";
+import moment from "moment";
 
 export default {
   name: "Chat",
@@ -45,7 +46,7 @@ export default {
             id: document.id,
             name: document.data().name,
             content: document.data().content,
-            timestamp: document.data().timestamp
+            timestamp: moment(document.data().timestamp).format("lll")
           });
         }
       });
@@ -65,6 +66,24 @@ export default {
 
 .chat .time {
   display: block;
-  font-size: 1.2em;
+  font-size: 1em;
+}
+
+.messages {
+  max-height: 300px;
+  overflow: auto;
+}
+
+.messages::-webkit-scrollbar {
+  width: 4px;
+}
+
+::-webkit-scrollbar-track {
+  background: rgb(187, 187, 187);
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #009688;
 }
 </style>
